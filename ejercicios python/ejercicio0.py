1864bfd749584e739ac0f022616af98e52d78eba
redpostmanrd <dariotito18@gmail.com>
2022-08-09 16:38:58 -0400

    Main ejercicio 0

---
+++ ejercicios python/ejercicio0.py
@@ -0,0 +1,11 @@
+""""
+Print the Python version to the console.
+
+"""
+import sys
+print("\n the python version is:")
+print(sys.version[0:6])
+print("\n")
+
+# solution by book 
+# print(sys.version.split()[0])
